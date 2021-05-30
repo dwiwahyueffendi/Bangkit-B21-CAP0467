a7ff8f85929c523caaef922012ad2901918ac851
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-05-30 15:44:11 +0700

    UI sign in & sign up

---
+++ Android/Capstone Project/app/src/main/java/com/example/capstoneproject/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstoneproject.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstoneproject.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
